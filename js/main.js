0146fc62c17ac768099dc4201d1958fe9cf1de13
sandesh <sandesh>
2022-02-28 17:04:12 +0545

    responsive

--- js/main.js
+++ js/main.js
@@ -1,59 +1,104 @@
 $('.slider').slick({
     infinite: true,
-    slidesToShow: 6,
+    slidesToShow: 3,
     slidesToScroll: 3,
     speed: 300,
     autoplay: true,
     prevArrow: '<i  class="far fa-arrow-left previous_arrow"></i>',
     nextArrow: '<i  class="far fa-arrow-right  next_arrow"></i>',
+    mobileFirst: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            centerMode: false
+        }
+    }]
   });
 
   $('.place_slider').slick({
   infinite: true,
-  slidesToShow: 4,
+  slidesToShow: 2,
   // dots: true,
   cssEase: 'linear',
   slidesToScroll: 1,
   // autoplay: true,
   // autoplaySpeed: 200,
   prevArrow: null,
-  nextArrow: null
+  nextArrow: null,
+  mobileFirst: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 4,
+            slidesToScroll: 1,
+            centerMode: false
+        }
+    }]
 });
 
 $('.latest_slider').slick({
   infinite: true,
-  slidesToShow: 3,
+  slidesToShow: 2,
   // dots: true,
   cssEase: 'linear',
   slidesToScroll: 1,
   // autoplay: true,
   // autoplaySpeed: 900,
   prevArrow: null,
-  nextArrow: null
+  nextArrow: null,
+  mobileFirst: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            centerMode: false
+        }
+    }]
 });
 
 $('.brand_slider').slick({
   infinite: true,
-  slidesToShow: 5,
+  slidesToShow: 3,
   // dots: true,
   cssEase: 'linear',
   slidesToScroll: 1,
   autoplay: true,
   // autoplaySpeed: 400,
   prevArrow: null,
-  nextArrow: null
+  nextArrow: null,
+  mobileFirst: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 5,
+            slidesToScroll: 1,
+            centerMode: false
+        }
+    }]
 });
 
 $('.testimonial').slick({
   infinite: true,
-  slidesToShow: 2,
+  slidesToShow: 1,
   // dots: true,
   cssEase: 'linear',
   slidesToScroll: 1,
   autoplay: true,
   // autoplaySpeed: 400,
   prevArrow: null,
-  nextArrow: null
+  nextArrow: null,
+  mobileFirst: true,
+    responsive: [{
+        breakpoint: 1024,
+        settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            centerMode: false
+        }
+    }]
 });
 
 //ANimation part
